2d51d1afb6b9b89311a1d1259caa800996afb97c
Brian Yee <bay001@tscc-login1.sdsc.edu>
2017-03-21 16:09:17 -0700

    first commit

---
+++ base_package/base_module.py
@@ -0,0 +1,52 @@
+"""
+basic module to use as an example.
+"""
+import argparse
+
+
+def add_two_numbers(a, b):
+    """
+
+    Args:
+        a: int
+            first number to add
+        b: int
+            second number to add
+    Returns:
+        c: int
+            addition between a and b
+    """
+
+    c = a + b
+    return c
+
+
+def main():
+    """
+    Main program.
+
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--firstnum", # use full name (optional) with double-dash
+        required=True, # specify required arguments
+        type=int,
+        default=0
+    )
+    parser.add_argument(
+        "--secondnum",
+        required=True,
+        type=int,
+        default=0
+    )
+    args = parser.parse_args()
+
+    first_num = args.firstnum
+    second_num = args.secondnum
+
+    added = add_two_numbers(a=first_num, b=second_num)
+    print("{} + {} = {}".format(first_num, second_num, added))
+
+if __name__ == "__main__":
+    main()
